4bab40f83e921534d876ccb2129b679865ef954c
DamonLindsay <damonides@gmail.com>
2023-09-24 17:11:51 +1000

    Change secret number to be read from a .txt file

--- week_03_practice/demo_guessing.py
+++ week_03_practice/demo_guessing.py
@@ -1,17 +1,25 @@
 """
 Guessing Game Demo
 """
-import random
+FILENAME = "secret.txt"
 
 
 def main():
     """This program is a guessing game, where a random number is generated the user attempts to guess it."""
-    secret = random.randint(1, 10)
+    secret = load_number(FILENAME)
     guess = int(input("Guess: "))
     while guess != secret:
         print("Incorrect guess.")
         guess = int(input("Guess: "))
     print("You got it!")
+
+
+def load_number(filename):
+    """Load an integer from file filename."""
+    infile = open(filename, "r")
+    number = int(infile.read())
+    infile.close()
+    return number
 
 
 main()
