a24d04aed03fa778f42227d4fc88b6cb07f0fd54
DamonLindsay <damonides@gmail.com>
2023-10-02 17:21:16 +1000

    Update main function with list comprehension

--- week_04_practice/strings_seminar_activity.py
+++ week_04_practice/strings_seminar_activity.py
@@ -2,17 +2,20 @@
 Strings - Seminar Activity
 """
 
-
 # Given a string called text, like: text = "This is a sentence"
 # Write a list comprehension that produces a list of the words that have > 3 characters.
 # Example: print(long_words)
 # Should output: ['This', 'sentence']
+MINIMUM_WORD_LENGTH = 3
+
 
 def main():
-    """This program will write a list comprehension that produces a list of the words that have > 3 characters."""
-    text = "This is a sentence"
+    """This program will write a list comprehension that produces a list of the words
+    that have > LONG_WORD_MINIMUM characters."""
 
-    long_words = text.split()
+    text = "This is a sentence"
+    long_words = [word for word in text.split() if len(word) > MINIMUM_WORD_LENGTH]
+
     print(long_words)
 
 
