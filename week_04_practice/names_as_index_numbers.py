e2fc1a7e3166e717ecf062b6938775c90d8c3c00
DamonLindsay <damonides@gmail.com>
2023-10-02 17:07:53 +1000

    Update with functions with error checking

--- week_04_practice/names_as_index_numbers.py
+++ week_04_practice/names_as_index_numbers.py
@@ -10,9 +10,23 @@ def main():
     """This program will ask the user which name they want to display as a number, then display it."""
     names = ["Damon", "Camryn", "Liam", "Maddison", "Noah", "Natalie", "Craig", "Jordan"]
 
-    number = int(input("Enter number, up to 8: "))
+    selected_name = get_name_by_number(names)
+    print(selected_name)
 
-    print(names[number])
+
+def get_name_by_number(sequence):
+    """Return the name of the specified index - 1."""
+    is_valid_input = False
+    while not is_valid_input:
+        try:
+            number = int(input(f"Enter number, up to {len(sequence)}: "))
+            if number <= 0:
+                print("Invalid number.  Number must be greater than 0.")
+            else:
+                is_valid_input = True
+                return sequence[number - 1]
+        except IndexError:
+            print("IndexError. List index out of range.")
 
 
 main()
