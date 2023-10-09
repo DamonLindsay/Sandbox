45de3cfd950da4c4f5d47bd37c1121806f7ef03e
DamonLindsay <damonides@gmail.com>
2023-10-10 08:58:22 +1000

    Add 2 ways to iterate through dictionaries

---
+++ week_05_practice/learning_dictionaries.py
@@ -0,0 +1,11 @@
+"""
+Lecture Demo 05 - Dictionaries
+"""
+
+name_to_age = {"Billy": 21, "Jan": 34, "Sven": 56}
+for name, age in name_to_age.items():
+    print(f"{name} is {age}")
+
+
+# for name in name_to_age:
+#     print(f"{name} is {name_to_age[name]}")
