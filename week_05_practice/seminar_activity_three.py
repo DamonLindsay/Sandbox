9b0a6291d9eb1a306a3939e84e84de3fb99a06b7
DamonLindsay <damonides@gmail.com>
2023-10-10 16:13:20 +1000

    Add main function to seminar_activity_three.py

--- week_05_practice/seminar_activity_three.py
+++ week_05_practice/seminar_activity_three.py
@@ -3,13 +3,20 @@ Write code to prompt the user for a new name and age, add these to the dictionar
 then display all the data nicely.
 """
 
-name_to_age = {"Bill": 21, "Jane": 4, "Sven": 56}
 
-name = input("Name: ")
-age = int(input("Age: "))
+def main():
+    """This program will get a name and age from the user, add it to the dictionary and then
+    print the contents of the dictionary."""
+    name_to_age = {"Bill": 21, "Jane": 4, "Sven": 56}
 
-name_to_age[name] = age
-max_length = max(len(name) for name in list(name_to_age.keys()))
+    name = input("Name: ")
+    age = int(input("Age: "))
 
-for name, age in name_to_age.items():
-    print(f"{name:{max_length}} - {age:4}")
+    name_to_age[name] = age
+    max_length = max(len(name) for name in list(name_to_age.keys()))
+
+    for name, age in name_to_age.items():
+        print(f"{name:{max_length}} - {age:4}")
+
+
+main()
