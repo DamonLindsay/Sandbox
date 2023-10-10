13bb71093dcd3132b5683584893d9c4add954d27
DamonLindsay <damonides@gmail.com>
2023-10-10 16:10:36 +1000

    Update seminar_activity.py with proper reverse sorting

--- week_05_practice/seminar_activity.py
+++ week_05_practice/seminar_activity.py
@@ -2,7 +2,6 @@
 WakaTIme
 """
 
-# Unfinished, Sorted by Name in Reverse, needs to be sorted by Score.  Also could use variable for formatting.
 name_to_score = {"Derek": 7, "Xavier": 80, "Bob": 612, "Chantanelle": 9}
 
 
@@ -11,7 +10,7 @@ def main():
     max_value_length = len(str(max(name_to_score.values())))
     max_key_length = max((len(name) for name in name_to_score.keys()))
 
-    for name, score in sorted(name_to_score.items(), reverse=True):
+    for name, score in sorted(name_to_score.items(), key=lambda x: x[1], reverse=True):
         print(f"{name:{max_key_length}} = {score:{max_value_length}}")
 
 
