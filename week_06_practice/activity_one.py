88ae827047a10d5549cd5c35f9b1b328e5ed7d3e
DamonLindsay <damonides@gmail.com>
2023-10-27 15:50:49 +1100

    Update activity_one.py and add product.py

--- week_06_practice/activity_one.py
+++ week_06_practice/activity_one.py
@@ -2,6 +2,6 @@
 
 products = [["Phone", 340, False], ["PC", 1420.95, True], ["Plant", 24.5, True]]
 
-on_sale_products = [product for product in products if product[2] is True]
+on_sale_products = [product for product in products if product[2]]
 
 print(on_sale_products)
