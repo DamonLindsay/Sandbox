e298eb48bd2e42031f99eae49a1d608784ce76c7
DamonLindsay <damonides@gmail.com>
2023-10-27 16:27:40 +1100

    Add the_bits.py to week_06_practice

---
+++ week_06_practice/the_bits.py
@@ -0,0 +1,13 @@
+"""The Bits(Names)"""
+
+from kivy.app import App
+from kivy.app import Widget
+
+
+class HelloWorld(App):
+    def build(self):
+        self.root = Widget()
+        return self.root
+
+
+HelloWorld().run()
