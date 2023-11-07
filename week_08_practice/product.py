af2b1670b0b3847307c4f52aba91f993c8eb6eec
DamonLindsay <damonides@gmail.com>
2023-11-07 16:44:48 +1000

    Add Product class module

---
+++ week_08_practice/product.py
@@ -0,0 +1,8 @@
+"""Product class module"""
+
+
+class Product:
+    def __init__(self, name="", price=0.0):
+        """Initialize the Product object."""
+        self.name = name
+        self.price = price
