ce711b804dfd249331beed858f17617c0a6088a0
DamonLindsay <damonides@gmail.com>
2023-11-07 17:09:05 +1000

    Update product.py to have error checking when loading products

--- week_08_practice/product.py
+++ week_08_practice/product.py
@@ -16,11 +16,18 @@ class ProductCollection:
 
     def load_products(self, filename):
         """Load the products from the specified file and append them to a list."""
-        with open(filename) as input_file:
-            data = json.load(input_file)
-            for item in data:
-                product = Product(item["name"], item["price"])
-                self.products.append(product)
+        try:
+            with open(filename) as input_file:
+                data = json.load(input_file)
+                for item in data:
+                    product = Product(item["name"], item["price"])
+                    self.products.append(product)
+        except FileNotFoundError as e:
+            print(f"File not found: {filename}.  Error: {e}.")
+        except json.JSONDecodeError as e:
+            print(f"Error decoding JSON in file: {filename}. Error: {e}.")
+        except Exception as e:
+            print(f"An error occurred while loading products from {filename}. Error: {e}.")
 
     def get_products(self):
         """Return the list of products."""
