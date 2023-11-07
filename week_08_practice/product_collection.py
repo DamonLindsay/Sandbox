de88d4dd4e0e6bd455c9c2237931fbcde3726325
DamonLindsay <damonides@gmail.com>
2023-11-07 16:47:36 +1000

    Add Product Collection class module

---
+++ week_08_practice/product_collection.py
@@ -0,0 +1,11 @@
+"""Product Collection module"""
+
+
+class ProductCollection:
+    def __init__(self):
+        """Initialize the Product Collection object."""
+        self.products = []
+
+    def get_products(self):
+        """Return the list of products."""
+        return self.products
