1fbba049ce625a7859eefaa374e288ca4d95c615
DamonLindsay <damonides@gmail.com>
2023-11-22 17:44:56 +1000

    Add additional info to displayed page

--- league_of_legends_wikipedia_kivy/champion_info_app.py
+++ league_of_legends_wikipedia_kivy/champion_info_app.py
@@ -28,23 +28,35 @@ class ChampionInfoApp(App):
         """Search for champion information and update the UI."""
         champion_input = self.root.ids.champion_input
         info_label = self.root.ids.info_label
+        title_label = self.root.ids.title_label
+        url_label = self.root.ids.url_label
 
         if champion_input.text:
             champion_name = champion_input.text
             try:
+                page = wikipedia.page(champion_name)
                 champion_info = wikipedia.summary(champion_name)
                 info_label.text = champion_info
+                title_label.text = f"Title: {page.title}"
+                url_label.text = f"URL: {page.url}"
             except wikipedia.exceptions.DisambiguationError as e:
                 # If there are multiple matches, display the first one
                 self.show_popup("Disambiguation Error",
                                 f"Multiple matches found.  Displaying the first one: {e.options[0]}")
                 champion_info = wikipedia.summary(e.options[0])
                 info_label.text = champion_info
+                title_label.text = ""
+                url_label.text = ""
             except wikipedia.exceptions.PageError:
                 self.show_popup("Page Error", "Champion not found on Wikipedia.")
                 info_label.text = ""
+                title_label.text = ""
+                url_label.text = ""
         else:
             info_label.text = "Please enter a champion name."
+            info_label.text = ""
+            title_label.text = ""
+            url_label.text = ""
 
     def show_popup(self, title, content):
         """Show a popup with a given title and content."""
@@ -54,6 +66,8 @@ class ChampionInfoApp(App):
     def clear_info(self):
         """Clear champion information."""
         self.root.ids.info_label.text = ""
+        self.root.ids.url_label.text = ""
+        self.root.ids.title_label.text = ""
         self.root.ids.champion_input.text = ""
 
 
