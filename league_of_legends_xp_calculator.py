157af63b924d334a26bd3c4ecb9a50589bd324a7
DamonLindsay <damonides@gmail.com>
2023-11-26 20:13:17 +1000

    Add main part of program

--- league_of_legends_xp_calculator.py
+++ league_of_legends_xp_calculator.py
@@ -4,12 +4,19 @@ League of Legends XP Calculator
 
 
 def main():
-    """"""
-    pass
+    """Main function to run the League of Legends XP Calculator program."""
+    print("League of Legends XP Calculator")
+    target_level = int(input("Enter the target level: "))
+
+    if target_level <= 0:
+        print("Please enter a valid target level greater than 0.")
+    else:
+        total_xp = calculate_xp_for_level(target_level)
+        print(f"The total XP required to reach level {target_level} is {total_xp}.")
 
 
 def calculate_xp_for_level(level):
-    """"""
+    """Calculate the total XP required to reach the given level in League of Legends."""
     # Base case: XP required for level 1 is 0
     if level == 1:
         return 0
