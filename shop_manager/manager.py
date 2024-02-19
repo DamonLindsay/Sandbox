cf10e522b70271a0f1103417e4b447d7d44af353
DamonLindsay <damonides@gmail.com>
2024-02-19 17:50:35 +1000

    Add manager.py class file

---
+++ shop_manager/manager.py
@@ -0,0 +1,19 @@
+"""
+Manager class file
+"""
+
+from employee import Employee
+
+
+class Manager(Employee):
+    """Class representing a manager."""
+
+    def __init__(self, name, employee_id, salary, bonus):
+        """Initalizes a Manager object."""
+        super().__init__(name, employee_id)
+        self.salary = salary
+        self.bonus = bonus
+
+    def calculate_pay(self):
+        """Calculates the pay of the manager."""
+        return self.salary + self.bonus
