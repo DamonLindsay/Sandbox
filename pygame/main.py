698954ced039aafa93e992398ef54655a56f9603
DamonLindsay <damonides@gmail.com>
2024-02-23 16:15:49 +1000

    Add Tilemap

--- pygame/main.py
+++ pygame/main.py
@@ -11,6 +11,14 @@ class Game:
         self.clock = pygame.time.Clock()
         self.running = True
 
+    def createTilemap(self):
+        for i, row in enumerate(tilemap):
+            for j, column in enumerate(row):
+                if column == "B":
+                    Block(self, j, i)
+                if column == "P":
+                    Player(self, j, i)
+
     def new(self):
         """A new game starts."""
         self.playing = True
@@ -20,7 +28,7 @@ class Game:
         self.enemies = pygame.sprite.LayeredUpdates()
         self.attacks = pygame.sprite.LayeredUpdates()
 
-        self.player = Player(self, 1, 2)
+        self.createTilemap()
 
     def events(self):
         """Game loop events."""
